5ec63ee528d66d4bcfcc24ebdb364c49884425ca
ArseniiKarov <156718450+ArseniiKarov@users.noreply.github.com>
2024-05-11 19:22:48 +0300

    Add files via upload
    если бы вы знали как мне похуй вы бы заплакали

---
+++ src/pages/InfoGraph/GraphPage.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Line } from 'react-chartjs-2';
+
+function GraphPage() {
+  const { id } = useParams();
+  const [chartData, setChartData] = useState({});
+
+  useEffect(() => {
+    axios.get(`api/v1/gettest/${id}`)
+      .then(response => {
+        const { data } = response;
+        const labels = data.map(item => item.time);
+        const maleData = data.map(item => item.maleAccuracy);
+        const femaleData = data.map(item => item.femaleAccuracy);
+        setChartData({
+          labels,
+          datasets: [
+            {
+              label: 'Мужчины',
+              data: maleData,
+              borderColor: 'blue',
+              backgroundColor: 'rgba(0, 0, 0, 0.1)',
+            }
+          ],
+          datasets: [
+            {
+              label: 'Женщины',
+              data: femaleData,
+              borderColor: 'blue',
+              backgroundColor: 'rgba(0, 0, 0, 0.1)',
+            }
+          ]
+        });
+      })
+      .catch(error => console.error('Ошибка получения данных:', error));
+  }, [id]);
+
+  return (
+    <div>
+      <h1>Тест {id}</h1>
+      <Line data={chartData} />
+    </div>
+  );
+}
+
+export default GraphPage;
