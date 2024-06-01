06437ae05c037aa3975d0977409af03e0a8809c2
He11oThere <korol.a04@mail.ru>
2024-06-01 23:49:08 +0300

    Добавлены два теста

--- src/pages/ResultsOfTests/ResultsOfTests.jsx
+++ src/pages/ResultsOfTests/ResultsOfTests.jsx
@@ -14,7 +14,9 @@ const ResultsOfTests = () => {
         { test_id: 28, name: 'Движущийся круг', path: '/ResultsOfPersonTests'},
         { test_id: 7, name: 'Три движущийхся круга', path: '/ResultsOfPersonTests'},
         { test_id: 5, name: 'Тест мышления простой', path: '/ResultsOfPersonTests' },
-        { test_id: 6, name: 'Тест мышления сложный', path: '/ResultsOfPersonTests' }
+        { test_id: 6, name: 'Тест мышления сложный', path: '/ResultsOfPersonTests' },
+        { test_id: 4, name: 'Простой трекинг', path: '/ResultsOfPersonTests'},
+        { test_id: 7, name: 'Преследование круга', path: '/ResultsOfPersonTests'}
     ];
 
     return (
