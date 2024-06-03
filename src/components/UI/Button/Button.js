016d8624cb0033289777cbdad02fd1c900f9cebf
Arman Toreniyazov <armanokka@mail.ru>
2024-06-03 17:50:23 +0300

    feat: button and logo animations

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import cs from './Button.module.css'
 const Button = ({onClick, type, ...props}) => {
     return (
-        <button type={type} onClick={onClick} className={cs.button}>
+        <button type={type} onClick={onClick} className={cs.button + " " + cs.btn}>
             {props.children}
         </button>
     );
