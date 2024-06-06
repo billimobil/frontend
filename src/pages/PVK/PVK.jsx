00a6140b6248e22e65271285e37eb0e9661a4876
Arman Toreniyazov <armanokka@mail.ru>
2024-06-06 20:32:54 +0300

    fix: PVK

--- src/pages/PVK/PVK.jsx
+++ src/pages/PVK/PVK.jsx
@@ -3,53 +3,60 @@ import cs from './PVK.module.css';
 import Button from '../../components/UI/Button/Button';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import profession from "../Profession/Profession";
 
 const Pvk = ({user}) => {
-    const { id } = useParams();
-    // const [profession, setProfession] = useState(null);
-    const [error, setError] = useState('');
-    const [clicked, setClicked] = useState([]);
-    var pvk = ["Креативность", "Способность к зрительным представлениям",
-        "Умственная работоспособность", "Аккуратность", "Глазомер: линейный, угловой, объемный", "Переключаемость внимания", "Образность",
-        "Предусмотрительность", "Умение подмечать незначительные изменения", "Способность к распознаванию небольших отклонений параметров по визуальным признакам",
-        "Оперативность", "Способность к переводу образа в словесное состояние", "Способность планировать свою деятельность во времени",
-        "Зрительная оценка размеров предметов", "Способность к пространственному воображению", "Способность к воссозданию образа по словесному описанию",
-        "Способность к распределению внимания между несколькими видами деятельность", "Предметность"]
+    const [response, setResponse] = useState()
+    const [error, setError] = useState('')
+    useEffect(()=>{
+        axios.get("http://188.225.74.17:8080/api/v1/getPVKList", {
+            params: {
+                session_token: user.session_token,
+            }
+        }).then(resp=>{
+            console.log(resp.data.data)
+            setResponse(resp.data.data)
+        }).catch(e=>{
+            setError(e.response.data.error)
+        })
+    }, [])
 
     return (
         <div className={cs.wrapper}>
             {error ? (
                 <div className="error">{error}</div>
-            ) : (
-                <>
-                    <h1 className={cs.head}>Результаты вашего тестирования</h1>
-
-                    <div className={cs.match}>
-                        <div className={cs.match__block}>
-                            <div className={cs.match__block__item}>Ваша совместимость с профессией: Веб-дизайнер</div>
-                            <b><div className={cs.match__block__item}>{getRandomInt(30, 80)}%</div></b>
+            ) : <></>
+            }
+            <h1 className={cs.head}>Результаты вашего тестирования</h1>
+            {
+                response ?
+                    <>
+                        <div className={cs.match}>
+                            {
+                                Object.entries(response.professions_match).map(([profession, score]) => (
+                                    <div className={cs.match__block}>
+                                        <div className={cs.match__block__item}>Ваша совместимость с профессией: {profession}</div>
+                                        <b><div className={cs.match__block__item}>{score}%</div></b>
+                                    </div>
+                                ))
+                            }
                         </div>
-                        <div className={cs.match__block}>
-                            <div className={cs.match__block__item}>Ваша совместимость с профессией: Веб-дизайнер</div>
-                            <b><div className={cs.match__block__item}>{getRandomInt(30, 80)}%</div></b>
-                        </div>
-                        <div className={cs.match__block}>
-                            <div className={cs.match__block__item}>Ваша совместимость с профессией: Веб-дизайнер</div>
-                            <b><div className={cs.match__block__item}>{getRandomInt(30, 80)}%</div></b>
-                        </div>
-                    </div>
 
+                        <div className={cs.pvk__block}>
+                            {
+                                Object.entries(response.pvk_match).map(([profession, score]) => (
+                                    <div className={cs.pvk}>
+                                        <p className={`${cs.pvk__name} ${cs.border}`}>{profession}</p>
+                                        <p>{score}%</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </>
+                    :
+                    <></>
+            }
 
-                    <div className={cs.pvk__block}>
-                        {pvk.map((elem) => (
-                            <div className={cs.pvk}>
-                                <p className={`${cs.pvk__name} ${cs.border}`}>{elem}</p>
-                                <p>{getRandomInt(50, 70)}%</p>
-                            </div>
-                        ))}
-                    </div>
-                </>
-            )}
         </div>
     );
 };
